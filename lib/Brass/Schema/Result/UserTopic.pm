86b3cbe843feb9e3c5576a4db1c5639c0431622e
Andy Beverley <a.beverley@ctrlo.com>
2016-12-18 23:37:03 +0000

    Add document access restrictions to schema

---
+++ lib/Brass/Schema/Result/UserTopic.pm
@@ -0,0 +1,30 @@
+use utf8;
+package Brass::Schema::Result::UserTopic;
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+__PACKAGE__->load_components("InflateColumn::DateTime");
+__PACKAGE__->table("user_topic");
+
+__PACKAGE__->add_columns(
+  "id",
+  { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
+  "user",
+  { data_type => "integer", is_foreign_key => 1, is_nullable => 0 },
+  "topic",
+  { data_type => "integer", is_nullable => 0 },
+);
+
+__PACKAGE__->set_primary_key("id");
+
+__PACKAGE__->belongs_to(
+  "user",
+  "Brass::Schema::Result::User",
+  { id => "user" },
+  { is_deferrable => 1, on_delete => "NO ACTION", on_update => "NO ACTION" },
+);
+
+1;
