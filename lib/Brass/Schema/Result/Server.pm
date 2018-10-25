5ed1f1c85a69d00b267f17456d005d945a9bfe1d
Andy Beverley <a.beverley@ctrlo.com>
2018-10-25 14:09:01 +0000

    Add production flag to database

--- lib/Brass/Schema/Result/Server.pm
+++ lib/Brass/Schema/Result/Server.pm
@@ -114,6 +114,8 @@ __PACKAGE__->add_columns(
   { data_type => "text", is_nullable => 1 },
   "notes",
   { data_type => "text", is_nullable => 1 },
+  "is_production",
+  { data_type => "smallint", is_nullable => 0, default_value => 0 },
 );
 
 =head1 PRIMARY KEY
