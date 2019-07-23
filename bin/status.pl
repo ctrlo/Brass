1babbdc201d998329e6b0a4f70939144ee1d6dfb
Andy Beverley <a.beverley@ctrlo.com>
2019-07-23 14:37:59 +0100

    Show certificates before backups in status report

--- bin/status.pl
+++ bin/status.pl
@@ -18,33 +18,10 @@ use Dancer2::Plugin::LogReport;
 
 my $schema = schema;
 
-my $servers = Brass::Config::Servers->new(schema => $schema);
-
-my $output = "Server backup observations:\n";
-$output   .= "===========================\n";
-my @messages;
-foreach my $server (@{$servers->all})
-{
-    next unless $server->has_type('backup');
-    if ($server->backup_status ne 'Identical' || !$server->backup_verify_time)
-    {
-        push @messages, sprinti "Server {name} most recent backup unsuccessful",
-            name => $server->name;
-    }
-    elsif ($server->backup_verify_time < DateTime->now->subtract(days => 5))
-    {
-        push @messages, sprinti "Last backup verification of server {name} was {date}",
-            name => $server->name, date => $server->backup_verify_time;
-    }
-}
-$output .= "$_\n" foreach @messages;
-$output .= "Nothing to report\n" if !@messages;
-
-$output .= "\n";
-$output .= "SSL certificate observations\n";
+my $output .= "SSL certificate observations\n";
 $output .= "============================\n";
 my $certs = Brass::Config::Certs->new(schema => $schema);
-@messages = ();
+my @messages;
 foreach my $cert (@{$certs->all})
 {
     $cert->expiry or next;
@@ -57,6 +34,30 @@ foreach my $cert (@{$certs->all})
     {
         push @messages, sprinti "Certificate {name} expires in less than 7 days on {date}",
             name => $cert->cn, date => $cert->expiry->ymd;
+    }
+}
+$output .= "$_\n" foreach @messages;
+$output .= "Nothing to report\n" if !@messages;
+
+$output .= "\n";
+
+my $servers = Brass::Config::Servers->new(schema => $schema);
+
+$output .= "Server backup observations:\n";
+$output .= "===========================\n";
+@messages = ();
+foreach my $server (@{$servers->all})
+{
+    next unless $server->has_type('backup');
+    if ($server->backup_status ne 'Identical' || !$server->backup_verify_time)
+    {
+        push @messages, sprinti "Server {name} most recent backup unsuccessful",
+            name => $server->name;
+    }
+    elsif ($server->backup_verify_time < DateTime->now->subtract(days => 5))
+    {
+        push @messages, sprinti "Last backup verification of server {name} was {date}",
+            name => $server->name, date => $server->backup_verify_time;
     }
 }
 $output .= "$_\n" foreach @messages;
