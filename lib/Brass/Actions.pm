f88c304ca086a1e8d26ae33594c2ad522b130d8b
Tom Hukins <tom@eborcom.com>
2023-11-06 13:47:43 +0000

    Simplify Brass::Actions
    Using a better data structure to specify rules requires less code.

--- lib/Brass/Actions.pm
+++ lib/Brass/Actions.pm
@@ -3,51 +3,43 @@ package Brass::Actions;
 use strict;
 use warnings;
 
-my @allowed_action = (
-    {
-        name     => 'admonitor',
+my %allowed_action = (
+    admonitor => {
         username => 0,
     },
-    {
-        name     => 'backups-gpg',
+    'backups-gpg' => {
         username => 1,
     },
-    {
-        name     => 'backups-s3',
+    'backups-s3' => {
         username => 1,
     },
-    {
-        name     => 'disk',
+    disk => {
         username => 0,
     },
-    {
-        name     => 'sqldb',
+    sqldb => {
         username => 1,
     },
-    {
-        name     => 'system',
+    system => {
         username => 1,
     },
-    {
-        name     => 'wazuh',
+    wazuh => {
         username => 1,
     },
 );
 
 sub allowed_actions {
-    return map $_->{name}, @allowed_action;
+    return keys %allowed_action;
 }
-
-my %requires_username = map { $_->{name} => $_->{username} } @allowed_action;
 
 sub is_allowed_action {
     my $action_name = $_[0];
-    return exists $requires_username{$action_name};
+    return exists $allowed_action{$action_name};
 }
 
 sub action_requires_pwd {
     my $action_name = $_[0];
-    return $requires_username{$action_name};
+    return exists $allowed_action{$action_name}
+        && $allowed_action{$action_name}{username};
 }
 
 1;
